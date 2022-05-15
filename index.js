717a782446ad46a543679b337e4107c6943bdb8e
amiraatalla <amiraatalla63@gmail.com>
2022-05-15 14:29:29 +0200

    .gitignore

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 3000l
+dotenv.config();
 
 const app = express();
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
+
