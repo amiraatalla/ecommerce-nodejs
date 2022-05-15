fe5e5b4fb3e60ef73244b4fb53116e39db7e141f
amiraatalla <amiraatalla63@gmail.com>
2022-05-15 16:03:37 +0200

    connected to database successfully

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const PORT = process.env.PORT || 3000l
+const PORT = process.env.PORT || 3000;
 dotenv.config();
-
 const app = express();
 
 
-app.listen(PORT, ()=>{
+mongoose.connect(process.env.MONGO_DB_URL)
+    .then(() => console.log('Mongo DB connected Successfully!'))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
 
