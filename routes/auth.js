80d109c786d61ce33c5fc6147f4f9cad51d5daac
amiraatalla <amiraatalla63@gmail.com>
2022-05-19 23:56:31 +0200

    update user

--- routes/auth.js
+++ routes/auth.js
@@ -26,19 +26,19 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.body.username });
+        const user = await User.findOne({ email: req.body.email });
         !user && res.status(400).json('Wrong Credentials');
 
         const validated = await bcrypt.compare(req.body.password, user.password);
         !validated && res.status(422), json('Wrong Password');
 
-        const { password, ...others } = user;
+        const { password, ...others } = user._doc;
         res.status(200).json(others);
     }
     catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 module.exports = router;
 
