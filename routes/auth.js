5d5d378771691eba4d322a057bd3a4998401eb6f
amiraatalla <amiraatalla63@gmail.com>
2022-05-20 00:10:12 +0200

    index

--- routes/auth.js
+++ routes/auth.js
@@ -24,6 +24,7 @@ router.post('/register', async (req, res) => {
 
 //login api 
 
+//it dose not work successfully
 router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email });
