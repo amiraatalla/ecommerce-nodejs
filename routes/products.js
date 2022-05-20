95cc515d3b5e4236ceed8413aadbe7bd3a815002
amiraatalla <amiraatalla63@gmail.com>
2022-05-20 13:34:20 +0200

    create a product

---
+++ routes/products.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const Product = require('../models/Product');
+const { json } = require('express/lib/response');
+
+
+//create product
+
+router.post('/create/product', async (req, res) => {
+    const newProduct = new Product(req.body);
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    }catech(err){
+        res.status(500), json(err);
+    }
+});
