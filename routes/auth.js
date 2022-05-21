1eb6afd780c9bf975e4a5852be2d40ca83ee4e04
Amira Atalla <52547252+amiraatalla@users.noreply.github.com>
2022-05-21 21:03:22 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -24,19 +24,25 @@ router.post('/register', async (req, res) => {
 
 //login api 
 
-//it dose not work successfully
+
 router.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({ email: req.body.email });
-        !user && res.status(400).json('Wrong Credentials');
+        const user = await User.findOne({
+            email: req.body.email
+        });
+        !user && res.status(404).json("email is not found");
 
-        const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(422), json('Wrong Password');
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(422).json("wrong credentials!");
 
         const { password, ...others } = user._doc;
-        res.status(200).json(others);
-    }
-    catch (err) {
+        res.status(200).json({
+            'message': 'Logged in successfully',
+            'user': others
+        });
+
+    } catch (err) {
         res.status(500).json(err);
     }
 });
